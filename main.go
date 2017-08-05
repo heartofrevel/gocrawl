97cb57ac0896ce941e2f0594122c8046580da491
Anshul <k3rn3l@live.com>
2017-08-05 23:42:47 +0530

    Output:JSON

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ import (
 	"golang.org/x/net/html"
 )
 
+type urlFound struct {
+	Count     int      `json:"count"`
+	Links     []string `json:"links"`
+	Images    []string `json:"images"`
+	Documents []string `json:"docs"`
+}
+
 var extensionList = []string{".pdf", ".doc", ".docx", ".ppt", ".jpeg", ".jpg", ".png"}
 
 func getReference(token html.Token) (flag bool, url string) {
@@ -84,7 +91,7 @@ func crawler(c echo.Context, urlRec string, feed chan string, urlList *[]string,
 					if strings.Contains(urlHref, URL.Host) {
 						*urlList = append(*urlList, urlHref)
 						fmt.Println(urlHref)
-						c.String(http.StatusOK, urlHref+"\n")
+						// c.String(http.StatusOK, urlHref+"\n")Documents
 						if !checkExt(filepath.Ext(urlHref)) {
 							wg.Add(1)
 							go crawler(c, urlHref, feed, urlList, wg)
@@ -96,20 +103,61 @@ func crawler(c echo.Context, urlRec string, feed chan string, urlList *[]string,
 	}
 }
 
-func scrape(c echo.Context) error {
+func scrapePOST(c echo.Context) error {
 	var urlList []string
+	urlSession := urlFound{}
 	var wg sync.WaitGroup
 	urlParam := c.FormValue("url")
 	feed := make(chan string, 1000)
 	wg.Add(1)
 	go crawler(c, urlParam, feed, &urlList, &wg)
 	wg.Wait()
-	return c.String(http.StatusOK, "Complete")
+	var count = 0
+	for _, url := range urlList {
+		if filepath.Ext(url) == ".jpg" || filepath.Ext(url) == ".jpeg" || filepath.Ext(url) == ".png" {
+			urlSession.Images = append(urlSession.Images, url)
+		} else if filepath.Ext(url) == ".doc" || filepath.Ext(url) == ".docx" || filepath.Ext(url) == ".pdf" || filepath.Ext(url) == ".ppt" {
+			urlSession.Documents = append(urlSession.Documents, url)
+		} else {
+			urlSession.Links = append(urlSession.Links, url)
+		}
+		count = count + 1
+	}
+	urlSession.Count = count
+	// jsonResp, _ := json.Marshal(urlSession)
+	// fmt.Print(urlSession)
+	return c.JSON(http.StatusOK, urlSession)
+}
+
+func scrapeGET(c echo.Context) error {
+	var urlList []string
+	urlSession := &urlFound{}
+	var wg sync.WaitGroup
+	urlParam := c.QueryParam("url")
+	feed := make(chan string, 1000)
+	wg.Add(1)
+	go crawler(c, urlParam, feed, &urlList, &wg)
+	wg.Wait()
+	var count = 0
+	for _, url := range urlList {
+		if filepath.Ext(url) == ".jpg" || filepath.Ext(url) == ".jpeg" || filepath.Ext(url) == ".png" {
+			urlSession.Images = append(urlSession.Images, url)
+		} else if filepath.Ext(url) == ".doc" || filepath.Ext(url) == ".docx" || filepath.Ext(url) == ".pdf" || filepath.Ext(url) == ".ppt" {
+			urlSession.Documents = append(urlSession.Documents, url)
+		} else {
+			urlSession.Links = append(urlSession.Links, url)
+		}
+		count = count + 1
+	}
+	urlSession.Count = count
+	// jsonResp, _ := json.Marshal(urlSession)
+	return c.JSON(http.StatusOK, urlSession)
 }
 
 func main() {
 	fmt.Println("Starting Web Scraper API")
 	e := echo.New()
-	e.POST("/scraper", scrape)
+	e.POST("/scraper", scrapePOST)
+	e.GET("/scraper", scrapeGET)
 	e.Start(":8001")
 }
